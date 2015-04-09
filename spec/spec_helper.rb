de8b63fa16ee8e765007bbd23360c48ee2ca8de6
Andreas Hoffmann <furizaa@gmail.com>
2015-04-09 19:49:23 +0200

    Try to get coverage running in circle ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
+if ENV['CIRCLE_ARTIFACTS']
+  require 'simplecov'
+  dir = File.join('..', '..', '..', ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
 module Helpers
   def json_response
     ActiveSupport::JSON.decode(response.body)
