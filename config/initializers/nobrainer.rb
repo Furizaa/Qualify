0a1503d59aa3d2333f1d7c430c34fd85a93d3fa6
Andreas Hoffmann <furizaa@gmail.com>
2015-04-20 21:48:48 +0200

    Add specs for database and table management

--- config/initializers/nobrainer.rb
+++ config/initializers/nobrainer.rb
@@ -15,7 +15,7 @@ NoBrainer.configure do |config|
   # * RETHINKDB_URL, RDB_URL
   # * RETHINKDB_HOST, RETHINKDB_PORT, RETHINKDB_DB, RETHINKDB_AUTH
   # * RDB_HOST, RDB_PORT, RDB_DB, RDB_AUTH
-  config.rethinkdb_url = ENV['RETHINK_DB']
+  config.rethinkdb_url = ENV['rethink_db']
 
   # NoBrainer uses logger to emit debugging information.
   # The default logger is the Rails logger if run with Rails,
---
+++ spec/lib/tenancy_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+require 'tenancy'
+
+RSpec.describe Tenancy do
+
+  after :each do
+    NoBrainer.db_list.each { |db| NoBrainer.db_drop(db) unless db == 'rethinkdb' }
+  end
+
+  let(:schema) { FactoryGirl::create(:schema) }
+  let(:impl) { Class.new { include Tenancy } }
+
+  it 'creates database and table' do
+    impl.new.create_tenant_schema(schema)
+    expect(NoBrainer.db_list).to include(schema.account.uuid)
+    NoBrainer.with_database(schema.account.uuid) do
+      expect(NoBrainer.table_list).to include(schema.uuid)
+    end
+    expect(NoBrainer.db_list.length).to be(2) # also counts system table "rethinkdb"
+    NoBrainer.db_drop(schema.account.uuid)
+  end
+
+  it 'creates database and table only once' do
+    impl.new.create_tenant_schema(schema)
+    impl.new.create_tenant_schema(schema)
+    expect(NoBrainer.db_list).to include(schema.account.uuid)
+    NoBrainer.with_database(schema.account.uuid) do
+      expect(NoBrainer.table_list.length).to be(1)
+    end
+    expect(NoBrainer.db_list.length).to be(2) # also counts system table "rethinkdb"
+    NoBrainer.db_drop(schema.account.uuid)
+  end
+
+  it 'destroys table' do
+    impl.new.create_tenant_schema(schema)
+    impl.new.destroy_tenant_schema(schema)
+    NoBrainer.with_database(schema.account.uuid) do
+      expect(NoBrainer.table_list.length).to be(0)
+    end
+    expect(NoBrainer.db_list.length).to be(2) # also counts system table "rethinkdb"
+    NoBrainer.db_drop(schema.account.uuid)
+  end
+
+  it 'destroys database' do
+    impl.new.create_tenant_schema(schema)
+    impl.new.destroy_tenant_schema(schema)
+
+    account = schema.account
+    account.schemas = []
+    impl.new.clean_tenant_databases_for(account)
+    expect(NoBrainer.db_list.length).to be(1) # also counts system table "rethinkdb"
+  end
+end
